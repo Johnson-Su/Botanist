1da745b8c1759f010a9d711d1115acfc4759598e
Amy <amyh102@gmail.com>
2021-05-01 02:14:07 -0600

    nlp entity analysis

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-
-const prefix = '>';
-
 const fs = require('fs');
+const language = require('@google-cloud/language');
+
+const client = new Discord.Client();
+const nlp_client = new language.LanguageServiceClient();
+
+const prefix = '-';
 
 client.commands = new Discord.Collection();
 
@@ -19,8 +21,37 @@ client.once('ready', () => {
     console.log('Botanist is online!');
 });
 
+
+
 client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    async function getEntities(text) {
+        message.channel.send("repeat:" + text);
+        // NLP entity analysis
+        const document = {
+            content: text,
+            type: 'PLAIN_TEXT',
+        };
+
+        // Detects entities in the document
+        const [result] = await nlp_client.analyzeEntities({document});
+        const entities = result.entities;
+    
+        message.channel.send('Entities:');
+        entities.forEach(entity => {
+            if(entity.name.toLowerCase() !== "bot"){
+                message.channel.send(entity.name);
+                message.channel.send(` - Type: ${entity.type}, Salience: ${entity.salience}`);
+                if (entity.metadata && entity.metadata.wikipedia_url) {
+                    message.channel.send(` - Wikipedia URL: ${entity.metadata.wikipedia_url}`);
+                }
+            }
+        });
+    }
+
+    const text = message.content;
+    getEntities(text);
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
@@ -30,4 +61,8 @@ client.on('message', message =>{
     } 
 });
 
-client.login('ODM3ODc2MTYyNzcxNTUwMjk5.YIy6_A.insrVdFlX319u6n4LDBZkN8l96g');
+var token = require('./token.js');
+BOT_TOKEN = token.BOT_TOKEN;
+client.login(BOT_TOKEN);
+
+

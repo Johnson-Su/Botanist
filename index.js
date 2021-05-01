e6ae98f098e9018afea8476a35a8a66183f760ad
Amy <amyh102@gmail.com>
2021-05-01 03:53:54 -0600

    add firebsae stuff

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const language = require('@google-cloud/language');
+// const admin = require('firebase-admin');
+// var serviceAccount = require("./service_account.json");
 
 const client = new Discord.Client();
 const nlp_client = new language.LanguageServiceClient();
+
+// var refreshToken = 'AIzaSyCwR7ySw5QkY6r1zpFhaF3VJhAJD0yuLR4'; // Get refresh token from OAuth2 flow
+// const functions = require('firebase-functions');
+// admin.initializeApp(functions.config().firebase);
+
+// const db = admin.firestore();
 
 const prefix = '-';
 
@@ -27,7 +35,7 @@ client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
     async function getEntities(text) {
-        message.channel.send("repeat:" + text);
+        message.channel.send("repeat: " + text); // testing
         // NLP entity analysis
         const document = {
             content: text,
@@ -37,7 +45,14 @@ client.on('message', message =>{
         // Detects entities in the document
         const [result] = await nlp_client.analyzeEntities({document});
         const entities = result.entities;
-    
+        // var array_len = entities.length;
+        // console.log(entities[0]);
+        // async function addToFirebase(){
+        //     const res = await db.collection('Request').doc('Length').set(entities[0]);
+        //     console.log(res);
+        // }
+        // addToFirebase();
+
         message.channel.send('Entities:');
         entities.forEach(entity => {
             if(entity.name.toLowerCase() !== "bot"){
@@ -46,10 +61,12 @@ client.on('message', message =>{
                 if (entity.metadata && entity.metadata.wikipedia_url) {
                     message.channel.send(` - Wikipedia URL: ${entity.metadata.wikipedia_url}`);
                 }
-            }
+            } 
+            // else{
+            //     array_len = array_len - 1;
+            // }
         });
     }
-
     const text = message.content;
     getEntities(text);
 
